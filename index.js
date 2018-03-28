44c3747b521717e4680dfeff01b2d823265c102f
Edy Segura <ednardo@inatel.br>
2018-03-27 23:07:25 -0300

    Update indentation

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
 const eventBus = document.getElementById('event-bus')
 
 function delegate(event) {
-    if(event.target !== event.currentTarget) {
-        output(event.target.id)
-    }
-    event.stopPropagation()
+  if (event.target !== event.currentTarget) {
+    output(event.target.id)
+  }
+  event.stopPropagation()
 }
 
 function output(message) {
-    const out = document.querySelector('output')
-    out.innerHTML = message + ' was clicked'
+  const out = document.querySelector('output')
+  out.innerHTML = message + ' was clicked'
 }
 
 eventBus.addEventListener('click', delegate)
