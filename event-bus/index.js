87feb6060c52bd0acdc11ce9eb411fb2b049693f
Edy Segura <edysegura@gmail.com>
2018-11-19 23:15:12 -0200

    Refactoring event-bus example

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,21 +1,20 @@
-
-const eventBus = document.getElementById('event-bus')
-
 function delegate(event) {
   event.stopPropagation()
+
   if (event.target !== event.currentTarget) {
-    output(event.target.id)
+    showOutput(event.target.id)
   }
+
   console.log('Target: ', event.target)
   console.log('Current Target: ', event.currentTarget)
 }
 
-function output(message) {
-  const out = document.querySelector('output')
-  out.textContent = message + ' was clicked'
+function showOutput(message) {
+  const output = document.querySelector('output')
+  output.textContent = message + ' was clicked'
 }
 
-function generateElements() {
+function generateElements(eventBus) {
   for (let i = 1; i < 201; i++) {
     let div = document.createElement('div')
     let divNumber = String(i).padStart(2, '0')
@@ -25,6 +24,8 @@ function generateElements() {
   }
 }
 
-generateElements()
+const eventBus = document.getElementById('event-bus')
 eventBus.addEventListener('click', delegate)
 document.addEventListener('click', () => console.log('document was clicked'))
+
+generateElements(eventBus)
